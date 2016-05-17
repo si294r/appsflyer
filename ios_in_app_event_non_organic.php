a712a3852859e7c1a327b961855f08984f8861c3
chairuddinsyah <chairuddinsyah@10.100.8.149>
2016-05-17 10:29:05 +0700

    Update Commit

---
+++ ios_in_app_event_non_organic.php
@@ -0,0 +1,7 @@
+<?php
+
+$input = file_get_contents("php://input");
+
+$filename = "./".date("YmdH").".ios_in_app_event_non_organic.txt";
+$fp = fopen($filename, 'a');
+fwrite($fp, $input . PHP_EOL);
