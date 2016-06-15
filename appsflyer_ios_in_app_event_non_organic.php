ea00c34ee6ffc6c69e56cbab34ae928a9cdbd0c9
chairuddinsyah <chairuddinsyah@10.100.8.149>
2016-06-15 16:31:20 +0700

    Update Commit

--- appsflyer_ios_in_app_event_non_organic.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$input = file_get_contents("php://input");
-
-$filename = "./".date("YmdH").".saved_in_app_event_non_organic.txt";
-$fp = fopen($filename, 'a');
-fwrite($fp, $input . PHP_EOL);
