f2bd616abed2ac35f5bcb3451bdfc463273858f8
chairuddinsyah <chairuddinsyah@10.100.8.149>
2016-06-29 10:15:48 +0700

    Update Commit

---
+++ run_manual.php.php
@@ -0,0 +1,16 @@
+<?php
+
+for ($i=31; $i>=1; $i--) {
+    $cmd = "php -f /var/www/html/appsflyer/daily_report_ios.php 2016-05-".str_pad($i, 2, "0", STR_PAD_RIGHT);
+    echo $cmd;; continue;
+    exec($cmd); 
+    sleep(60);
+}
+
+for ($i=31; $i>=1; $i--) {
+    $cmd = "php -f /var/www/html/appsflyer/partners_daily_report_ios.php 2016-05-".str_pad($i, 2, "0", STR_PAD_RIGHT);
+    echo $cmd;; continue;
+    exec($cmd); 
+    sleep(60);
+}
+
