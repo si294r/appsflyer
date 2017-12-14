3e817edd7f44d378055984a7eb773b68d4d2a771
chairuddinsyah <chairuddinsyah@10.100.8.149>
2017-12-14 15:45:34 +0700

    Update Commit

--- event_appsflyer_new_office.php
+++ event_appsflyer_new_office.php
@@ -12,23 +12,26 @@ if (isset($argv[2])) {
 } else {
     $date = date('Y-m-d', strtotime("-1 days"));
 }
-echo $date;
 
 $filename = "{$csv_prefix}$date.csv";
 
 $ls_output = [];
+echo $date."...";
 exec("aws s3 ls s3://apps-flyer/{$folder_s3}/$filename", $ls_output);
 
 if (count($ls_output) > 0) {
-    
+
+    echo "download...";
     exec("aws s3 cp s3://apps-flyer/{$folder_s3}/$filename $current_dir/$filename");
 
     $table_name = $tablename;
 
+    echo "delete...";
     $pcmd = "psql --host=$rhost --port=$rport --username=$ruser --no-password --echo-all $rdatabase  -c \"DELETE FROM {$table_name} WHERE install_time::date = '$date';\"";
     $output = array();
     exec($pcmd, $output);
 
+    echo "insert...".PHP_EOL;
     $pcmd = "psql --host=$rhost --port=$rport --username=$ruser --no-password --echo-all $rdatabase  -c \"\\COPY {$table_name} FROM '{$current_dir}/{$filename}' DELIMITER ',' NULL '' QUOTE '\\\"' CSV HEADER ;\"";
     exec($pcmd, $output);
     
@@ -36,5 +39,7 @@ if (count($ls_output) > 0) {
 
     unlink("$current_dir/$filename");
     
+} else {
+    echo "not found.".PHP_EOL;
 }
 
