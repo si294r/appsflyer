8048f9d67b36cdb47a71000d9f114725bbee0045
chairuddinsyah <chairuddinsyah@10.100.8.149>
2017-12-14 15:51:34 +0700

    Update Commit

--- event_appsflyer_new_office.php
+++ event_appsflyer_new_office.php
@@ -8,39 +8,64 @@ include 'config.php';
 load_config();
 
 if (isset($argv[2])) {
-    $date = $argv[2];
+//    $date = $argv[2];
+    $startdate = $argv[2];
+    if (isset($argv[3])) {
+        $enddate = $argv[3];
+        if ($enddate < $startdate) {
+            $enddate = $startdate;
+        }
+    } else {
+        $enddate = date('Y-m-d');
+    }
 } else {
-    $date = date('Y-m-d', strtotime("-1 days"));
+//    $date = date('Y-m-d');
+    $startdate = date('Y-m-d');
+    $enddate = date('Y-m-d');
 }
 
-$filename = "{$csv_prefix}$date.csv";
+$obj_date = DateTime::createFromFormat('Y-m-d', $startdate);
 
-$ls_output = [];
-echo $date."...";
-exec("aws s3 ls s3://apps-flyer/{$folder_s3}/$filename", $ls_output);
+while (true) {
 
-if (count($ls_output) > 0) {
-
-    echo "download...";
-    exec("aws s3 cp s3://apps-flyer/{$folder_s3}/$filename $current_dir/$filename");
-
-    $table_name = $tablename;
-
-    echo "delete...";
-    $pcmd = "psql --host=$rhost --port=$rport --username=$ruser --no-password --echo-all $rdatabase  -c \"DELETE FROM {$table_name} WHERE install_time::date = '$date';\"";
-    $output = array();
-    exec($pcmd, $output);
-
-    echo "insert...";
-    $pcmd = "psql --host=$rhost --port=$rport --username=$ruser --no-password --echo-all $rdatabase  -c \"\\COPY {$table_name} FROM '{$current_dir}/{$filename}' DELIMITER ',' NULL '' QUOTE '\\\"' CSV HEADER ;\"";
-    exec($pcmd, $output);
+    $date = $obj_date->format('Y-m-d');
     
-    echo "done".PHP_EOL;
-    echo implode(PHP_EOL, $output).PHP_EOL.PHP_EOL;
+    $filename = "{$csv_prefix}$date.csv";
 
-    unlink("$current_dir/$filename");
+    $ls_output = [];
+    echo $date."...";
+    exec("aws s3 ls s3://apps-flyer/{$folder_s3}/$filename", $ls_output);
+
+    if (count($ls_output) > 0) {
+
+        echo "download...";
+        exec("aws s3 cp s3://apps-flyer/{$folder_s3}/$filename $current_dir/$filename");
+
+        $table_name = $tablename;
+
+        echo "delete...";
+        $pcmd = "psql --host=$rhost --port=$rport --username=$ruser --no-password --echo-all $rdatabase  -c \"DELETE FROM {$table_name} WHERE install_time::date = '$date';\"";
+        $output = array();
+        exec($pcmd, $output);
+
+        echo "insert...";
+        $pcmd = "psql --host=$rhost --port=$rport --username=$ruser --no-password --echo-all $rdatabase  -c \"\\COPY {$table_name} FROM '{$current_dir}/{$filename}' DELIMITER ',' NULL '' QUOTE '\\\"' CSV HEADER ;\"";
+        exec($pcmd, $output);
+
+        echo "done".PHP_EOL;
+        echo implode(PHP_EOL, $output).PHP_EOL.PHP_EOL;
+
+        unlink("$current_dir/$filename");
+
+    } else {
+        echo "not found.".PHP_EOL.PHP_EOL;
+    }
+
+    if ($obj_date->format('Y-m-d') == $enddate) {
+        break;
+    } else {
+        $obj_date->add(new DateInterval("P1D"));
+    }
     
-} else {
-    echo "not found.".PHP_EOL.PHP_EOL;
 }
 
